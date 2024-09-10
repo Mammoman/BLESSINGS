751fc86bfe610a43f7fee87cea2cd1ba0cff6f54
daniel08seun@gmail.com <daniel08seun@gmail.com>
2024-09-10 19:08:02 +0100

    Add-Scroll

--- scripts/home.js
+++ scripts/home.js
@@ -1,2 +1,16 @@
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "+50px";  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+function showGiantImage(clickedImage) {
+  var giantImage = document.getElementById('giantImage');
+  var tempSrc = giantImage.src;
+  giantImage.src = clickedImage.src;
+  clickedImage.src = tempSrc;
+}
